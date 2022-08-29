100b163070acf5cef55dffdd62c6d38e1a4332ad
Maurits Bos <mb.brains@gmail.com>
2022-08-29 22:26:24 +0200

    tinyUrl implementation

---
+++ prisma/shortUrl.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+import { now } from "mongoose";
+import nanoid from "nanoid";
+
+const prisma = new PrismaClient();
+const expiryDays = 30;
+
+export async function getDestination(path:string) {
+    const shortUrl = await prisma.shortUrl.findUnique({
+        where: {
+            tiny_url: path
+        }
+    });
+
+    return shortUrl?.destination
+}
+
+export async function createShortUrl(destination: string) {
+    const short_url = await prisma.shortUrl.create({
+        data: {
+            destination: destination,
+            tiny_url: nanoid(7),
+        }
+    })
+
+    return short_url.tiny_url
+}
+
+export async function cleanUrls() {
+    const today: Date = new Date();
+    
+    
+    const res = await prisma.shortUrl.deleteMany({where : {
+        created: {
+            lt: new Date(today.getDate() + expiryDays)
+        }
+    }});
+
+    console.log(`deleted ${res.count} old `)
+}
--- util/constants.ts
+++ util/constants.ts
@@ -8,3 +8,8 @@ export const CONTRACT_CODEID = parseInt(
 export const SG721_CODEID = parseInt(process.env.NEXT_PUBLIC_SG721_CODEID!);
 
 export const NFT_API = process.env.NEXT_PUBLIC_NFT_API!;
+
+
+export const DB_NAME = process.env.DB_NAME!;
+export const DB_URL = process.env.DATABASE!;
+export const DB_COLLECTION_NAME = process.env.COLLECTION_NAME!;
